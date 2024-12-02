88f122b0ba426ac9e66ac52225087ec80a85d2e5
Shestopalov Ivan <shestopalov.ivan@1win.pro>
2024-12-02 21:57:56 +0300

    add default app file

--- src/dungeon_app.erl
+++ src/dungeon_app.erl
@@ -4,10 +4,19 @@
 
 -export([
   start/2,
+  start/0,
   stop/1
 ]).
 
 start(_Type, _StartArgs) ->
+  case dungeon_sup:start_link() of
+    {ok, Pid} ->
+      {ok, Pid};
+    Other -> 
+      {error, Other}
+    end.
+
+start() ->
   case dungeon_sup:start_link() of
     {ok, Pid} ->
       {ok, Pid};
